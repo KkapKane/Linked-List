0244f1bc2218aa2b0b23f54eb660658a7af5b696
Kane <LisaxKane@gmail.com>
2022-11-11 15:02:31 -0600

    updated the class constructor and added an append function

--- main.js
+++ main.js
@@ -1,15 +1,49 @@
 
 class LinkedList {
-    constructor(head) {
-        this.head = head;
-       
+    constructor(head = null) {
+        this.head = new Node(head);
+        this.tail = this.head;
+
+      
     }
-    Printlist() {
-    if (this.head == null) return;
-    console.log(this.head.value);
-        Printlist(this.head.next);
+    append(value) {
+        const newNode = new Node(value);
+        this.tail.next = newNode;
+        this.tail = newNode;
         
+        return this;
+
+    }
+    Printlist(head) {
+
+       let current = head;
+        while (current !== null) {
+            
+            
+            console.log(current.value);
+            current = current.next
+
+            return this;
         
+        }
+    }
+    size() {
+        let current = this.head;
+        let count = 0;
+        while (current !== null) {
+            count++;
+            current = current.next
+        }
+        return count;
+    }
+    tail() {
+        let tail = this.head;
+        if (tail) {
+            while (tail.next) {
+                tail = tail.next
+            }
+        }
+        return tail;
     }
 }
 
@@ -24,17 +58,12 @@ class Node {
 
 
 
-node1 = new Node(1)
-node2 = new Node(2)
-node3 = new Node(3)
-node4 = new Node(4)
-node5 = new Node(5)
-node6 = new Node(6)
+let list2 = new LinkedList(5)
 
-node1.next = node2;
-node2.next = node3;
-node3.next = node4;
-node4.next = node5;
-node5.next = node6;
-const list = new LinkedList(node1)
-list.Printlist()
+list2.append(3)
+list2.append(15)
+list2.append(73)
+
+
+console.log(list2.size())
+console.log(list2.tail)
